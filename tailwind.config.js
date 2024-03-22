c30e4e63b27d52333b822c0d027e1c2391d7fe1e
Axel Joshue Fiestas Santisteban <josue123452002@gmail.com>
2024-03-22 17:00:19 -0500

    feat: portfolio first version

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors:{
+        'dark-background':'#2A282B'
+      },
+    },
+  },
+  plugins: [],
+}
+
